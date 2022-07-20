5b6fec8b7dabca347b19f49e23799ddc92cd93b3
Almaz Žaparov <108575456+xyrcrrsbmjak@users.noreply.github.com>
2022-07-20 13:39:52 +0600

    Add getUsers helper to make request for getting users

---
+++ src/scripts/requests.js
@@ -0,0 +1,36 @@
+import { storage, setItem } from './ls.js'
+
+function request(url) {
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      Authorization: `token ${import.meta.env.VITE_GITHUB_PAT} `,
+    },
+  })
+}
+
+async function getUsers(page) {
+  try {
+    const response = await request(
+      `https://api.github.com/search/users?page=${page}&per_page=${storage.search.perPage}&sort=${storage.search.sort}&order=${storage.search.order}&q=${storage.search.username} in:username`
+    )
+
+    if (!response.ok) {
+      throw new Error(response.statusText)
+    }
+
+    const body = await response.json()
+
+    setItem('search', {
+      ...storage.search,
+      status: 'some',
+      items: body.items,
+      totalCount: body.total_count,
+      page,
+    })
+  } catch (error) {
+    setItem('search', { ...storage.search, page, status: 'error' })
+  }
+}
+
+export { request, getUsers }
