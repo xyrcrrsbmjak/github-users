b4254ed35e3c63892cdb5a6fe2547a8a8334da8b
Almaz Žaparov <108575456+xyrcrrsbmjak@users.noreply.github.com>
2022-07-20 10:40:12 +0600

    Add storage listeners, these are functions that can be executed when the storage is changed. 🛑 STOP USING state AS A STORAGE ALIAS

--- src/scripts/ls.js
+++ src/scripts/ls.js
@@ -1,13 +1,16 @@
 class LocalStorage {
   constructor() {
     this.storage = {}
+    this.listeners = {}
 
     for (const [key, value] of Object.entries(localStorage)) {
       this.storage[key] = JSON.parse(value)
+      this.listeners[key] = []
     }
 
     this.initStorage = this.initStorage.bind(this)
     this.setItem = this.setItem.bind(this)
+    this.addListener = this.addListener.bind(this)
   }
 
   initStorage(name, initial, clear = false) {
@@ -19,18 +22,29 @@ class LocalStorage {
 
     if (!isInStorage) {
       this.storage[name] = initial
+      this.listeners[name] = []
 
       localStorage.setItem(name, initial)
     }
+  }
+
+  addListener(name, func) {
+    this.listeners[name].push(func)
   }
 
   setItem(name, state) {
     this.storage[name] = state
 
     localStorage.setItem(name, JSON.stringify(state))
+
+    for (const key of Object.keys(this.listeners)) {
+      for (const func of this.listeners[key]) {
+        func()
+      }
+    }
   }
 }
 
-const { storage, initStorage, setItem } = new LocalStorage()
+const { storage, initStorage, addListener, setItem } = new LocalStorage()
 
-export { storage, initStorage, setItem }
+export { storage, initStorage, addListener, setItem }
