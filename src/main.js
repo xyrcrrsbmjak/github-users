2475947afb9dc25a30bdd873ad6b84ea5fd5223e
Almaz Žaparov <108575456+xyrcrrsbmjak@users.noreply.github.com>
2022-07-21 12:49:07 +0600

    Add page navigation

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import SearchPage from './scripts/search.js'
+import FavouritesPage from './scripts/favourites.js'
+import UserPage from './scripts/user.js'
 import { html } from './scripts/slowpoke.js'
 
 import './style.css'
 
-const app = html`<div class="container" id="app">${SearchPage}</div>`
+let page = null
+
+switch (window.location.pathname.replace('/', '')) {
+  case '':
+    page = SearchPage
+    break
+  case 'favourites':
+    page = FavouritesPage
+    break
+  default:
+    page = UserPage
+    break
+}
+
+const app = html`
+  <div class="container" id="app">
+    <header class="header">
+      <ul>
+        <li><a href="/">Search</a></li>
+        <li><a href="/favourites">Favourites</a></li>
+      </ul>
+    </header>
+    ${page}
+  </div>
+`
 
 document.body.appendChild(app)
